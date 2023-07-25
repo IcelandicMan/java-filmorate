cec6b1afe1fa3562ef9e05eee5f8c31114abed8f
Kostya V <larind93@gmail.com>
2023-07-25 15:32:26 +0500

    Добавлено логирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -22,26 +22,40 @@ public class DirectorController {
 
     @GetMapping()
     public List<Director> getDirectors() {
-        return filmService.getDirectors();
+        log.info("Запрос списка всех режиссёров");
+        List<Director> directors = filmService.getDirectors();
+        log.info("Список режиссёров получен");
+        return directors;
     }
 
     @GetMapping("/{id}")
     public Director getDirectorById(@PathVariable("id") Integer id) {
-        return filmService.getDirectorById(id);
+        log.info("Запрошен режиссёр с id = {}", id);
+        Director director = filmService.getDirectorById(id);
+        log.info("Режиссёр с id = {} получен: {}", id, director);
+        return director;
     }
 
     @PostMapping()
     public Director createDirector(@Valid @RequestBody Director director) {
-        return filmService.createDirector(director);
+        log.info("Запрос на создание режиссёра: {}", director);
+        Director newDirector = filmService.createDirector(filmService.createDirector(director));
+        log.info("Режиссёр создан: {}", director);
+        return newDirector;
     }
 
     @PutMapping()
     public Director updateDirector(@Valid @RequestBody Director director) {
-        return filmService.updateDirector(director);
+        log.info("Запрос на обновление режиссёра: {}", director);
+        Director updateDirector = filmService.updateDirector(director);
+        log.info("Режиссёр обновлён: {}", director);
+        return updateDirector;
     }
 
     @DeleteMapping("/{id}")
     public void deleteDirectorById(@PathVariable("id") Integer id) {
+        log.info("Запрос на удаление режиссёра с id: {}", id);
         filmService.deleteDirectorById(id);
+        log.info("Режиссёр удалён");
     }
 }
